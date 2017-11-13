9b360cd5fa8fb41e6a96d0ed61409609474e4700
doe300 <stadeldani@web.de>
2017-11-13 10:49:28 +0100

    Adds support for cl_ext_atomic_counters_32 extension

--- include/_extensions.h
+++ include/_extensions.h
@@ -8,6 +8,7 @@
 #define VC4CL_EXTENSIONS_H
 
 #include "_config.h"
+#include "_overloads.h"
 
 
 /*
@@ -41,6 +42,30 @@ uint arm_get_core_id(void)
 {
 	return 0;
 }
+
+/*
+ * 32-bit atomic counters
+ *
+ * Adds type
+ *  counter_32_t
+ * which is a 32-bit type for atomic counters. counter32_t can only be passed as kernel parameter and cannot be read/assigned.
+ *
+ * Adds functions
+ *  uint atomic_inc(counter32_t counter)
+ *  uint atomic_dec(counter32_t counter)
+ * increments/decrements the given counter32_t value atomically.
+ *
+ * NOTE: Since the syntax/semantics is exactly the same as for the uint version of the standard atomic_inc/atomic_dec functions, counter32_t is used as typedef to an uint pointer.
+ *
+ * See https://www.khronos.org/registry/OpenCL/extensions/ext/cl_ext_atomic_counters_32.txt
+ */
+#ifndef cl_ext_atomic_counters_32
+#define cl_ext_atomic_counters_32 1
+#endif
+typedef volatile __global uint* counter32_t;
+//just the prototypes, the implementations reside in _atomics.h
+uint atomic_inc(counter32_t counter) OVERLOADABLE;
+uint atomic_dec(counter32_t counter) OVERLOADABLE;
 
 #endif /* VC4CL_EXTENSIONS_H */
 
