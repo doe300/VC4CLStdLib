3c70f9a1c5261a07aa3e5c901250631a8c2bb75a
doe300 <stadeldani@web.de>
2018-03-11 11:08:46 +0100

    Removes implementation of OpenCL 2.x std-lib functions, fixes https://github.com/doe300/VC4C/issues/46

--- include/_work_items.h
+++ include/_work_items.h
@@ -50,29 +50,5 @@ INLINE size_t get_global_offset(uint dim) OVERLOADABLE CONST
 	return vc4cl_global_offset(dim);
 }
 
-/*
- * These functions are actually not part of OpenCL C 1.2, but are introduced in OpenCL C 2.0.
- * Anyway, they are useful helper, we can use for implementing the standard-library functions
- */
-/*
- * Returns the work-­items 1­‐dimensional local ID.
- */
-size_t get_local_linear_id(void) CONST; // prototype
-INLINE size_t get_local_linear_id() OVERLOADABLE CONST
-{
-	return vc4cl_mul24(vc4cl_mul24(vc4cl_local_id(2), vc4cl_local_size(1), VC4CL_UNSIGNED), vc4cl_bitcast_uint(vc4cl_local_size(0)), VC4CL_UNSIGNED) + vc4cl_mul24(vc4cl_local_id(1), vc4cl_local_size(0), VC4CL_UNSIGNED) + vc4cl_local_id(0);
-}
-
-/*
- * Returns the work-items 1-dimensional global ID.
- */
-size_t get_global_linear_id(void) CONST; // prototype
-size_t get_global_linear_id(void) OVERLOADABLE CONST
-{
-	return ((get_global_id(2) - get_global_offset(2)) * get_global_size(1) * get_global_size(0)) +
-		((get_global_id(1) - get_global_offset(1)) * get_global_size(0)) +
-		(get_global_id(0) - get_global_offset(0));
-}
-
 #endif /* VC4CL_WORK_ITEMS_H */
 
