dc7b63f4224f77dd4cfb5b025153e09c8c653f84
doe300 <stadeldani@web.de>
2018-08-25 08:25:08 +0200

    Adds support for ARM integer dot product extensions - cl_arm_integer_dot_product_int8 - cl_arm_integer_dot_product_accumulate_int8 - cl_arm_integer_dot_product_accumulate_int16

--- include/_extensions.h
+++ include/_extensions.h
@@ -9,6 +9,7 @@
 
 #include "_config.h"
 #include "_overloads.h"
+#include "_intrinsics.h"
 
 
 /*
@@ -66,6 +67,82 @@ typedef volatile __global uint* counter32_t;
 //just the prototypes, the implementations reside in _atomics.h
 uint atomic_inc(counter32_t counter) OVERLOADABLE;
 uint atomic_dec(counter32_t counter) OVERLOADABLE;
+
+/*
+ * Integer dot products
+ *
+ * Adds functions
+ *  int arm_dot(char4 a, char4 b)
+ *  uint arm_dot(uchar4 a, uchar4 b)
+ *  int arm_dot_acc(char4 a, char4 b, int acc)
+ *  uint arm_dot_acc(uchar4 a, uchar4 b, uint acc)
+ *  int arm_dot_acc(short2 a, short2 b, int acc)
+ *  uint arm_dot_acc(ushort2 a, ushort2 b, uint acc)
+ * calculate integer dot product (and additionally adds the scalar value)
+ *
+ * See https://www.khronos.org/registry/OpenCL/extensions/arm/cl_arm_integer_dot_product.txt
+ */
+#ifndef cl_arm_integer_dot_product_int8
+#define cl_arm_integer_dot_product_int8 1
+#endif
+#ifndef cl_arm_integer_dot_product_accumulate_int8
+#define cl_arm_integer_dot_product_accumulate_int8 1
+#endif
+#ifndef cl_arm_integer_dot_product_accumulate_int16
+#define cl_arm_integer_dot_product_accumulate_int16 1
+#endif
+
+// prototypes to prevent warnings
+int arm_dot(char4 a, char4 b)  OVERLOADABLE;
+uint arm_dot(uchar4 a, uchar4 b)  OVERLOADABLE;
+int arm_dot_acc(char4 a, char4 b, int acc) OVERLOADABLE;
+uint arm_dot_acc(uchar4 a, uchar4 b, uint acc) OVERLOADABLE;
+int arm_dot_acc(short2 a, short2 b, int acc) OVERLOADABLE;
+uint arm_dot_acc(ushort2 a, ushort2 b, uint acc) OVERLOADABLE;
+
+/**
+ * (a.x * b.x) + (a.y * b.y) + (a.z * b.z) + (a.w * b.w)
+ */
+int arm_dot(char4 a, char4 b)  OVERLOADABLE CONST
+{
+	int4 tmp = vc4cl_mul24(a, b, VC4CL_SIGNED);
+	return tmp.s0 + tmp.s1 + tmp.s2 + tmp.s3;
+}
+uint arm_dot(uchar4 a, uchar4 b)  OVERLOADABLE CONST
+{
+	uint4 tmp = vc4cl_mul24(a, b, VC4CL_UNSIGNED);
+	return tmp.s0 + tmp.s1 + tmp.s2 + tmp.s3;
+}
+
+/**
+ * acc + [ (a.x * b.x) + (a.y * b.y) + (a.z * b.z) + (a.w * b.w) ]
+ */
+int arm_dot_acc(char4 a, char4 b, int acc) OVERLOADABLE CONST
+{
+	int4 tmp = vc4cl_mul24(a, b, VC4CL_SIGNED);
+	return acc + tmp.s0 + tmp.s1 + tmp.s2 + tmp.s3;
+}
+
+uint arm_dot_acc(uchar4 a, uchar4 b, uint acc) OVERLOADABLE CONST
+{
+	uint4 tmp = vc4cl_mul24(a, b, VC4CL_UNSIGNED);
+	return acc + tmp.s0 + tmp.s1 + tmp.s2 + tmp.s3;
+}
+
+/**
+ * acc + [ (a.x * b.x) + (a.y * b.y) ]
+ */
+int arm_dot_acc(short2 a, short2 b, int acc) OVERLOADABLE CONST
+{
+	int2 tmp = vc4cl_mul24(a, b, VC4CL_SIGNED);
+	return acc + tmp.s0 + tmp.s1;
+}
+
+uint arm_dot_acc(ushort2 a, ushort2 b, uint acc) OVERLOADABLE CONST
+{
+	uint2 tmp = vc4cl_mul24(a, b, VC4CL_UNSIGNED);
+	return acc + tmp.s0 + tmp.s1;
+}
 
 #endif /* VC4CL_EXTENSIONS_H */
 
